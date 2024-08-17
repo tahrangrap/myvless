57fff9bda9f61d3e50170da16242cb0cbff0619e
tahrangrap <tahrangrap@gmail.com>
2024-08-17 06:17:13 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 sudo apt-get update
 sudo apt-get install -y jq openssl qrencode
 
-curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/default.json > config.json
+curl -s https://raw.githubusercontent.com/tahrangrap/myvless/master/default.json > config.json
 
 # Extract the desired variables using jq
 name=$(jq -r '.name' config.json)
+myuid=$(jq -r '.myuid' config.json)
 email=$(jq -r '.email' config.json)
 port=$(jq -r '.port' config.json)
 sni=$(jq -r '.sni' config.json)
@@ -15,7 +16,7 @@ path=$(jq -r '.path' config.json)
 
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install --beta
 
-json=$(curl -s https://raw.githubusercontent.com/sajjaddg/xray-reality/master/config.json)
+json=$(curl -s https://raw.githubusercontent.com/tahrangrap/myvless/master/config.json)
 
 keys=$(xray x25519)
 pk=$(echo "$keys" | awk '/Private key:/ {print $3}')
@@ -24,18 +25,18 @@ serverIp=$(curl -s ipv4.wtfismyip.com/text)
 uuid=$(xray uuid)
 shortId=$(openssl rand -hex 8)
 
-url="vless://$uuid@$serverIp:$port?type=http&security=reality&encryption=none&pbk=$pub&fp=chrome&path=$path&sni=$sni&sid=$shortId#$name"
+url="vless://$myuid@$serverIp:$port?type=http&security=reality&encryption=none&pbk=$pub&fp=chrome&path=$path&sni=$sni&sid=$shortId#$name"
 
 newJson=$(echo "$json" | jq \
     --arg pk "$pk" \
-    --arg uuid "$uuid" \
+    --arg uuid "$myuid" \
     --arg port "$port" \
     --arg sni "$sni" \
     --arg path "$path" \
     --arg email "$email" \
     '.inbounds[0].port= '"$(expr "$port")"' |
      .inbounds[0].settings.clients[0].email = $email |
-     .inbounds[0].settings.clients[0].id = $uuid |
+     .inbounds[0].settings.clients[0].id = $myuid |
      .inbounds[0].streamSettings.realitySettings.dest = $sni + ":443" |
      .inbounds[0].streamSettings.realitySettings.serverNames += ["'$sni'", "www.'$sni'"] |
      .inbounds[0].streamSettings.realitySettings.privateKey = $pk |
